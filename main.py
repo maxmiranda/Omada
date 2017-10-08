30fabb5ee53f150620a632c489dc203eaf7c00f3
Daniel Andrews <danielandrewsre@gmail.com>
2017-10-08 06:24:52 -0700

    votes

--- main.py
+++ main.py
@@ -80,11 +80,10 @@ def proposals():
 def groups():
     return render_template('groups.html')
 
-@app.route('/vote/<stock_id>/<approve>', methods=['GET', 'POST'])
-def vote(stock_id, approve):
+@app.route('/vote/<stock_id>/<buy>/<approve>', methods=['GET', 'POST'])
+def vote(stock_id, buy, approve):
     if request.method == 'POST':
-        return stock_id + approve
-        return mongo.vote(stock_id, buy, approve)
+        return mongo.vote(int(stock_id), int(buy), int(approve))
 
 @app.route('/search', methods=['GET'])
 def search():
