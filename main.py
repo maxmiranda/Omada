001b2d3ce10cac6816516ebd297e83e2560d05ed
Daniel Andrews <danielandrewsre@gmail.com>
2017-10-08 05:57:28 -0700

    votes

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def groups():
 
 @app.route('/vote', methods=["POST"])
 def vote():
-    return request
+    return request.form
     if request.method == 'POST':
         stock_id = request.form['id']
         buy = request.form['buy']
