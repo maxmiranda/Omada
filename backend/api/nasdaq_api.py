f2316011d833529498913b3aca4a0a9aa8dc521a
JeremyEphron <jeremyephron@gmail.com>
2017-10-07 20:00:18 -0700

    Nasdaq API mock data

---
+++ backend/api/nasdaq_api.py
@@ -0,0 +1,50 @@
+import json
+import websocket
+import threading
+import time
+
+class Nasdaq(object):
+
+    def __init__(self, symbol):
+        self.mock_data = []
+
+        self.symbol = symbol
+
+        url = 'ws://34.214.11.52/stream?symbol={}'.format(symbol)
+        ws = websocket.WebSocketApp(url,
+                                    on_message = self.on_message,
+                                    on_error = self.on_error,
+                                    on_close = self.on_close)
+        ws.on_open = self.on_open
+        ws.run_forever()
+
+
+    def on_message(self, ws, message):
+        self.mock_data.append(message)
+
+
+    @staticmethod
+    def on_error(ws, error):
+        print(error)
+
+
+    @staticmethod
+    def on_close(ws):
+        pass
+
+
+    @staticmethod
+    def on_open(ws):
+        def run():
+            ws.send("")
+            time.sleep(1)
+            ws.close()
+        threading.Thread(target=run).start()
+
+
+def simulate_data(symbol, callback):
+    data = Nasdaq(symbol).mock_data
+    for line in data:
+        line = json.loads(line)
+        callback([line["LastSale"], line["DateStamp"]])
+        time.sleep(1.5)
