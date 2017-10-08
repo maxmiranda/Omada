88d669162ddde47d6c0b3159a9b505e3c822495d
Daniel Andrews <danielandrewsre@gmail.com>
2017-10-08 06:33:41 -0700

    votes

--- backend/api/omada_db_api.py
+++ backend/api/omada_db_api.py
@@ -39,6 +39,8 @@ class OmadaDB(object):
     def update_stocks(self, stock_id, params):
         """Update stock instance with new params"""
 
+        print(params)
+
         return self.stocks.find_one_and_update({"id": stock_id}, params)
 
     def vote(self, stock_id, buy, approve):
